82e1f441967986d7032a205a3108bef99aa6527d
Your Name <your.email@example.com>
2025-03-28 13:16:26 +0530

    new code

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -4,5 +4,5 @@ const app = require('./src/app');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on port https://codereviewerbackend-hxejcyffg6ahbff8.eastasia-01.azurewebsites.net:${PORT}...`);
+  console.log(`Server is running on port - https://codereviewerbackend-hxejcyffg6ahbff8.eastasia-01.azurewebsites.net:${PORT}...`);
 });
